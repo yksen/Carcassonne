b1907c22c9e7aa2be7d806bdf855c09249d0ae75
yksen <selegakamil@gmail.com>
2021-04-10 13:10:19 +0200

    Mouse click and drag scrolling, new App class

--- main.py
+++ main.py
@@ -11,56 +11,74 @@ screen = pygame.display.set_mode((1600, 900))
 pygame.display.set_caption("Carcassonne")
 tileSet = pygame.image.load("tiles.png").convert()
 
-class Game:
+class App:
     def __init__(self):
-        self.hasStarted = 0
-    def start(self):
-        self.hasStarted = 1
-        menu.toggle()
-    def drawGame(self):
-        if self.hasStarted:
-            screen.fill((33, 17, 6))
-            if len(placedTiles) > 0:
-                for i in range(len(placedTiles)):
-                    screen.blit(placedTiles[i].surface, (placedTiles[i].posX, placedTiles[i].posY)) 
-    def drawMenu(self):
-        if menu.is_enabled():
-            menu.draw(screen)
-            menu.update(events)
-    def draw(self):
-        self.drawGame()
-        self.drawMenu()
-    class PlacedTile():
-        def __init__(self, id, posX, posY, rotation):
-            self.id = id
-            self.txtPosX = self.id.x
-            self.txtPosY = self.id.y
-            tileTxt = tileSet.subsurface(self.txtPosX, self.txtPosY, 256, 256)
-            self.surface = pygame.transform.scale(tileTxt, (128, 128))
-            self.posX = math.floor(posX / 128) * 128
-            self.posY = math.floor(posY / 128) * 128
-            self.rotation = rotation
-        def rotateTile(self):
-            pass
+        self.mouseClicked = 0
+        self.mouseMoving = 0
+    class Game:
+        def __init__(self):
+            self.hasStarted = 0
+            self.placedTiles = []
+            self.relativeX = 0
+            self.relativeY = 0
+        def start(self):
+            self.hasStarted = 1
+            menu.toggle()
+        def drawMenu(self):
+            if menu.is_enabled():
+                menu.draw(screen)
+                menu.update(events)
+        def drawGame(self):
+            if self.hasStarted:
+                screen.fill((33, 17, 6))
+                if len(self.placedTiles) > 0:
+                    for i in range(len(self.placedTiles)):
+                        screen.blit(self.placedTiles[i].surface, (self.placedTiles[i].posX + self.relativeX, self.placedTiles[i].posY + self.relativeY)) 
+        def drawAll(self):
+            self.drawGame()
+            self.drawMenu()
+        class PlacedTile:
+            def __init__(self, id, posX, posY, rotation):
+                self.id = id
+                self.txtPosX = self.id.x
+                self.txtPosY = self.id.y
+                tileTxt = tileSet.subsurface(self.txtPosX, self.txtPosY, 256, 256)
+                self.surface = pygame.transform.scale(tileTxt, (128, 128))
+                self.posX = math.floor(posX / 128) * 128
+                self.posY = math.floor(posY / 128) * 128
+                self.rotation = rotation
+            def rotateTile(self):
+                pass
 
-game = Game()
+carcassonne = App()
+game = carcassonne.Game()
 
 menu = pygame_menu.Menu('', 1600, 900, theme = pygame_menu.themes.THEME_DARK)
 menu.add.button('Play', game.start)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
-placedTiles = []
+
 
 while True:
     events = pygame.event.get()
     for event in events:
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()      
-        if event.type == pygame.MOUSEBUTTONUP and game.hasStarted:
-            placedTiles.append(game.PlacedTile(getattr(ts, 'tile' + str(random.randrange(1, 72))), pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 0))
+            sys.exit()              
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            carcassonne.mouseClicked = 1          
+        if game.hasStarted:
+            if event.type == pygame.MOUSEMOTION and carcassonne.mouseClicked:
+                carcassonne.mouseMoving = 1
+                game.relativeX += event.rel[0]
+                game.relativeY += event.rel[1]
+            if event.type == pygame.MOUSEBUTTONUP and not carcassonne.mouseMoving:            
+                game.placedTiles.append(game.PlacedTile(getattr(ts, 'tile' + str(random.randrange(1, 72))), pygame.mouse.get_pos()[0] - game.relativeX, pygame.mouse.get_pos()[1] - game.relativeY, 0))
+        if event.type == pygame.MOUSEBUTTONUP:
+            carcassonne.mouseClicked = 0
+            carcassonne.mouseMoving = 0  
 
-    game.draw()
-
+    game.drawAll()
     pygame.display.update()
+
     FramePerSec.tick(FPS)
