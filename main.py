53f2e804d07f33d82a53270c0a9fc18a4cbb7ff2
yksen <selegakamil@gmail.com>
2021-06-15 17:31:12 +0200

    Game settings menu, game state display

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pygame, sys, random, pygame_menu, random, math, copy
+import pygame, sys, random, pygame_menu, math, copy
 from pygame.locals import *
 import tileSets as ts
 
 pygame.init()
- 
+
 FPS = 144
 FramePerSec = pygame.time.Clock()
 
@@ -12,7 +12,14 @@ SCREEN_HEIGHT = 900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Carcassonne")
-tileSet = pygame.image.load("tiles.png").convert()
+
+tileSet = pygame.image.load("textures/tiles.png").convert()
+blueMeeple = pygame.image.load("textures/blue-meeple.png").convert_alpha()
+greenMeeple = pygame.image.load("textures/green-meeple.png").convert_alpha()
+redMeeple = pygame.image.load("textures/red-meeple.png").convert_alpha()
+yellowMeeple = pygame.image.load("textures/yellow-meeple.png").convert_alpha()
+
+font = pygame.font.SysFont('calibri', 24)
 
 class App:
     def __init__(self):
@@ -21,9 +28,10 @@ class App:
         self.mouseMoving = 0
     class Game:
         def __init__(self):
-            self.boardSize = 101
-            self.hasStarted = 0        
-            self.relativeX = -(math.floor(self.boardSize / 2) * 128) + math.floor((SCREEN_WIDTH / 128 / 2)) * 128
+            self.boardSize = 71
+            self.hasStarted = 0
+            self.gameStateMenuWidth = 300
+            self.relativeX = -(math.floor(self.boardSize / 2) * 128) + math.floor(((SCREEN_WIDTH - self.gameStateMenuWidth) / 128 / 2)) * 128
             self.relativeY = -(math.floor(self.boardSize / 2) * 128) + math.floor((SCREEN_HEIGHT / 128 / 2)) * 128
             self.placedTiles = []
             for i in range(self.boardSize):
@@ -34,10 +42,31 @@ class App:
             self.tileStack = []
             self.drawNextTile = 1
             self.currentRotation = 0
+            #####
+            self.playerTurn = 0
+            self.playerCount = 2
+            self.playerNames = []
+            self.playerColors = (blueMeeple, greenMeeple, redMeeple, yellowMeeple)
+            self.playerMeeples = [7, 7, 7, 7]
+            self.playerPoints = [0, 10, 110, 0]
         def start(self):
             self.hasStarted = 1
+            self.setPlayerNames()
             self.gameSetup()
+        def launchSettingsMenu(self):
             menu.toggle()
+            settingsMenu.toggle()
+        def setPlayerCount(self, x, value):
+            self.playerCount = value
+            for n, widget in enumerate(settingsMenu.get_widgets()):
+                if n != 0 and n != 1:
+                    settingsMenu.remove_widget(widget)
+            for i in range(self.playerCount):
+                settingsMenu.add.text_input("", default = "Player " + str(i + 1) + " name", repeat_keys_interval_ms=10, maxchar=20)
+            settingsMenu.add.button('Play', game.start)
+        def setPlayerNames(self):
+            for i in range(self.playerCount):
+                self.playerNames.append(settingsMenu.get_widgets()[2 + i].get_value())
         def gameSetup(self):
             tileOrder = []
             for i in range(72):
@@ -47,14 +76,17 @@ class App:
             for i in range(71):
                 randIndex = random.randint(0, len(tileOrder) - 1)
                 self.tileStack.append(tileOrder[randIndex])      
-                tileOrder.pop(randIndex)      
-            self.placedTiles[50][50] = self.Tile(ts.tile36, 50, 50)
+                tileOrder.pop(randIndex)   
+            halfBoardSize = math.floor(self.boardSize / 2)   
+            self.placedTiles[halfBoardSize][halfBoardSize] = self.Tile(ts.tile36, halfBoardSize, halfBoardSize)
             self.nextTile = self.Tile(getattr(ts, 'tile' + str(self.tileStack[0])), 0, 0)
             self.checkAvaiableSpots()
-        def drawMenu(self):
-            if menu.is_enabled():
-                menu.draw(screen)
-                menu.update(events)
+        def drawSettingsMenu(self):
+            settingsMenu.draw(screen)
+            settingsMenu.update(events)
+        def drawMenu(self):            
+            menu.draw(screen)
+            menu.update(events)
         def drawGame(self):
             if self.hasStarted:
                 screen.fill((77, 40, 0))
@@ -71,16 +103,41 @@ class App:
                         if 1 in self.availableSpots[i][j]:
                             x = i * 128 + math.floor(self.relativeX)
                             y = j * 128 + math.floor(self.relativeY)
-                            pygame.draw.rect(screen, (179, 89, 0), pygame.Rect(x, y, 128, 128))
+                            pygame.draw.rect(screen, (128, 64, 0), pygame.Rect(x, y, 128, 128))
                             if self.availableSpots[i][j][self.currentRotation] == True:
-                                pygame.draw.rect(screen, (0, 153, 0), pygame.Rect(x, y, 128, 128))                                                  
-                if self.drawNextTile:
-                    tileTxt = tileSet.subsurface(self.nextTile.txtPosX, self.nextTile.txtPosY, 256, 256)
-                    scaledTxt = pygame.transform.scale(tileTxt, (128, 128))
-                    screen.blit(pygame.transform.rotate(scaledTxt, -90 * self.nextTile.rotation), (0, 0))
-        def drawAll(self):
-            self.drawGame()
-            self.drawMenu()
+                                pygame.draw.rect(screen, (0, 153, 0), pygame.Rect(x, y, 128, 128))                                                                  
+        def drawGameState(self):
+            pygame.draw.rect(screen, (200, 115, 24), pygame.Rect(SCREEN_WIDTH - self.gameStateMenuWidth, 0, self.gameStateMenuWidth, SCREEN_HEIGHT))
+            pygame.draw.rect(screen, (77, 40, 0), pygame.Rect(SCREEN_WIDTH - 128 - ((self.gameStateMenuWidth - 128) / 2) - 4, 64 - 4, 128 + 2 * 4, 128 + 2 * 4))
+            text = font.render('Next tile:', True, (0, 0, 0))
+            screen.blit(text, text.get_rect(center=(SCREEN_WIDTH - self.gameStateMenuWidth / 2, 32)))
+            text = font.render(str(self.playerNames[self.playerTurn]) + "'s turn", True, (0, 0, 0))
+            screen.blit(text, text.get_rect(center=(SCREEN_WIDTH - self.gameStateMenuWidth / 2, SCREEN_HEIGHT - 32)))
+            if self.drawNextTile:
+                tileTxt = tileSet.subsurface(self.nextTile.txtPosX, self.nextTile.txtPosY, 256, 256)
+                scaledTxt = pygame.transform.scale(tileTxt, (128, 128))
+                screen.blit(pygame.transform.rotate(scaledTxt, -90 * self.nextTile.rotation), (SCREEN_WIDTH - 128 - ((self.gameStateMenuWidth - 128) / 2), 64))
+            for n, name in enumerate(self.playerNames):
+                infoX = SCREEN_WIDTH - self.gameStateMenuWidth + 16
+                infoY = 320 + n * 80
+                infoWidth = self.gameStateMenuWidth - 32
+                infoHeight = 64
+                pygame.draw.rect(screen, (252, 177, 3), pygame.Rect(infoX, infoY, infoWidth, infoHeight))
+                text = font.render(name, True, (0, 0, 0))
+                screen.blit(text, text.get_rect(center=(SCREEN_WIDTH - self.gameStateMenuWidth / 2 - 26, infoY + 18)))
+                text = font.render(str(self.playerPoints[n]), True, (0, 0, 0))
+                screen.blit(text, text.get_rect(center=(SCREEN_WIDTH - 44, infoY + infoHeight / 2 + 2)))
+                scaledTxt = pygame.transform.scale(self.playerColors[n], (24, 24))
+                for i in range(self.playerMeeples[n]):
+                    screen.blit(scaledTxt, (infoX + 12 + i * 28, infoY + infoHeight - 28))            
+        def drawAll(self):            
+            if menu.is_enabled():
+                self.drawMenu()
+            if settingsMenu.is_enabled():
+                self.drawSettingsMenu()
+            if self.hasStarted:
+                self.drawGame()
+                self.drawGameState()
         def placeTile(self, mousePos):                     
             if len(self.tileStack) > 0:                         
                 posX = mousePos[0] - self.relativeX
@@ -97,7 +154,7 @@ class App:
                         self.nextTile = self.Tile(getattr(ts, 'tile' + str(self.tileStack[0])), 0, 0)
                     else:
                         self.drawNextTile = 0                   
-                self.checkAvaiableSpots()                   
+                    self.checkAvaiableSpots()                   
         def checkAvaiableSpots(self):
             self.availableSpots.clear()
             for i in range(self.boardSize):
@@ -132,8 +189,7 @@ class App:
                                     fitsLeft = left is None or left.getTerrain('E') == terrainType
                                     fits = fits and fitsLeft
                             if fits:
-                                self.availableSpots[x][y][rot] = True    
-                            
+                                self.availableSpots[x][y][rot] = True                                
         def getTile(self, x, y):
             return self.placedTiles[x][y]
         class Tile:
@@ -180,8 +236,15 @@ app = App()
 game = app.Game()
 
 menu = pygame_menu.Menu('', SCREEN_WIDTH, SCREEN_HEIGHT, theme = pygame_menu.themes.THEME_DARK)
-menu.add.button('Play', game.start)
+menu.add.button('Play', game.launchSettingsMenu)
 menu.add.button('Quit', pygame_menu.events.EXIT)
+
+settingsMenu = pygame_menu.Menu('', SCREEN_WIDTH, SCREEN_HEIGHT, theme = pygame_menu.themes.THEME_DARK)
+settingsMenu.toggle()
+settingsMenu.add.selector('Player count: ', [('2', 2), ('3', 3), ('4', 4)], default = game.playerCount - 2, onchange = game.setPlayerCount)
+settingsMenu.add.label('Player names:')
+settingsMenu.add.button('Play', game.start)
+game.setPlayerCount('', 2)
 
 while True:
     events = pygame.event.get()
